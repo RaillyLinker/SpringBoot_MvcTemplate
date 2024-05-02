1a0fd3a8e2ccf5b0b6df26d92ded161753e13a12
RaillyLinker <raillylinker@gmail.com>
2024-05-02 10:41:01 +0900

    2-65 정렬 기준 추가

--- src/main/kotlin/com/raillylinker/springboot_mvc_template/ApplicationMain.kt
+++ src/main/kotlin/com/raillylinker/springboot_mvc_template/ApplicationMain.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.util.*
 
 @EnableScheduling // 스케쥴러 사용 설정
 @EnableAsync // 스케쥴러의 Async 사용 설정
@@ -11,5 +12,8 @@ import org.springframework.scheduling.annotation.EnableScheduling
 class ApplicationMain
 
 fun main(args: Array<String>) {
+    // 서버 타임존 변경
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+
     runApplication<ApplicationMain>(*args)
 }
