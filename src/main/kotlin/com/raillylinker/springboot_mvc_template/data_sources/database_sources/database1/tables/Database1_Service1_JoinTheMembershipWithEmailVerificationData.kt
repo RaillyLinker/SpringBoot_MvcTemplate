cc95801d6caea1997bb183bccf089360127e3600
RaillyLinker <raillylinker@gmail.com>
2024-05-20 14:47:55 +0900

    JPA Default Value 설정

--- src/main/kotlin/com/raillylinker/springboot_mvc_template/data_sources/database_sources/database1/tables/Database1_Service1_JoinTheMembershipWithEmailVerificationData.kt
+++ src/main/kotlin/com/raillylinker/springboot_mvc_template/data_sources/database_sources/database1/tables/Database1_Service1_JoinTheMembershipWithEmailVerificationData.kt
@@ -1,6 +1,7 @@
 package com.raillylinker.springboot_mvc_template.data_sources.database_sources.database1.tables
 
 import jakarta.persistence.*
+import org.hibernate.annotations.ColumnDefault
 import org.hibernate.annotations.Comment
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
@@ -39,6 +40,7 @@ class Database1_Service1_JoinTheMembershipWithEmailVerificationData(
     var rowUpdateDate: LocalDateTime? = null
 
     @Column(name = "row_delete_date_str", nullable = false, columnDefinition = "VARCHAR(50)")
+    @ColumnDefault("'-'")
     @Comment("행 삭제일(yyyy_MM_dd_T_HH_mm_ss_SSS_z, 삭제되지 않았다면 -)")
     var rowDeleteDateStr: String = "-"
 
