67606d42a629e34e2143536a184b5b5a9e2b9d80
raillylinker <raillylinker@gmail.com>
2024-06-12 21:28:39 +0900

    리펙토링

--- src/main/kotlin/com/raillylinker/springboot_mvc_template/ApplicationRuntimeConfigs.kt
+++ src/main/kotlin/com/raillylinker/springboot_mvc_template/ApplicationRuntimeConfigs.kt
@@ -6,13 +6,17 @@ import com.raillylinker.springboot_mvc_template.data_sources.database_sources.da
 import java.nio.file.Files
 import java.nio.file.Paths
 
-// [런타임에 변경 가능한 설정 정보를 모아둔 Object]
-// 이곳에 저장되어 있는 각 public 전역변수들은 런타임에 변경 가능한 설정 데이터를 의미합니다.
-// 아래 데이터를 이용하는 로직을 작성한다면, 런타임에 언제든 변경이 가능하다는 것을 인지한 상태로 개발하시고,
-// 런타임 설정을 변경 하려면 각 데이터 소스(파일, 데이터베이스, etc...)의 데이터를 수정한 후, 각 load 함수를 사용하여 설정 데이터를 갱신하면 됩니다.
-// 외부에서 갱신이 쉽도록, 갱신 함수는 C1 의 API 로 제공하면 좋습니다.
+/*
+     [런타임에 변경 가능한 설정 정보를 모아둔 Object]
+     이곳에 저장되어 있는 각 public 전역변수들은 런타임에 변경 가능한 설정 데이터를 의미합니다.
+     아래 데이터를 이용하는 로직을 작성한다면, 런타임에 언제든 변경이 가능하다는 것을 인지한 상태로 개발하시고,
+     런타임 설정을 변경 하려면 각 데이터 소스(파일, 데이터베이스, etc...)의 데이터를 수정한 후, 각 load 함수를 사용하여 설정 데이터를 갱신하면 됩니다.
+     외부에서 갱신이 쉽도록, 갱신 함수는 C1 의 API 로 제공하면 좋습니다.
+
+    !!!런타임 설정을 추가한다면, 아래에 추가, ApplicationMain 에서 load, C1 에 런타임 로딩 API 를 추가 해주면 됩니다.!!!
+ */
 object ApplicationRuntimeConfigs {
-    // (런타임 설정 데이터)
+    // (런타임 설정 데이터) - ./by_product_files/configs/runtime_config.json
     // 범용적인 런타임 설정 데이터는 이곳에 있습니다.
     var runtimeConfigFile: RuntimeConfigFile =
         // 설정 파일이 없을 때의 초기 설정
@@ -64,7 +68,7 @@ object ApplicationRuntimeConfigs {
 
 
     // -----------------------------------------------------------------------------------------------------------------
-    // (service1 런타임 설정 데이터)
+    // (service1 런타임 설정 데이터) - Database1_Service1_RuntimeConfigDataRepository
     // service1 의 런타임 설정 데이터는 이곳에 있습니다.
     var runtimeConfigDbService1: RuntimeConfigDbService1 =
         // 설정 파일이 없을 때의 초기 설정
