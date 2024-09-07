5d26e8e3cb5303f62ab34126fb95fa9b37edb885
RaillyLinker <raillylinker@gmail.com>
2024-09-07 12:19:32 +0900

    주석 수정

--- src/main/kotlin/com/raillylinker/springboot_mvc_template/ApplicationConstants.kt
+++ src/main/kotlin/com/raillylinker/springboot_mvc_template/ApplicationConstants.kt
@@ -5,7 +5,7 @@ import java.io.File
 import java.nio.file.Paths
 
 // (const 시점에서 사용하는 설정 변수 모음)
-// application.yml 과 동일한 역할을 하지만, const 시점에 조회하기 위해 모아둔 변수입니다.
+// application.yml 과 동일한 역할을 하지만, 프로세스에 코드를 로딩하는 시점에 조회하기 위해 모아둔 변수입니다.
 object ApplicationConstants {
     // (DatabaseConfig)
     // !!!본인의 패키지명 작성!!!
