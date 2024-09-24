448dd7f93f1988696bd357c57cae79b60472ba4e
RaillyLinker <raillylinker@gmail.com>
2024-09-25 04:46:40 +0900

    kafka 예시 수정

--- src/main/kotlin/com/raillylinker/springboot_mvc_template/kafka_consumers/KafkaConsumerForTestListeners.kt
+++ src/main/kotlin/com/raillylinker/springboot_mvc_template/kafka_consumers/KafkaConsumerForTestListeners.kt
@@ -24,30 +24,30 @@ class KafkaConsumerForTestListeners {
     // <공개 메소드 공간>
     // (testTopic1 에 대한 리스너)
     // containerFactory 에 kafka consumer container factory 이름 - KafkaConsumerConfig 의 Bean 함수명을 입력하세요
-    @KafkaListener(topics = ["testTopic1"], groupId = "group_0", containerFactory = "kafkaConsumerForTest")
+    @KafkaListener(topics = ["testTopic1"], containerFactory = "kafkaConsumerForTestGroup1")
     fun testTopic1Group0Listener(data: Any?) {
-        classLogger.info(">> testTopic1 group_0 : $data")
+        classLogger.info(">> testTopic1 group_1 : $data")
     }
 
     // (testTopic2 에 대한 리스너)
     // containerFactory 에 kafka consumer container factory 이름 - KafkaConsumerConfig 의 Bean 함수명을 입력하세요
-    @KafkaListener(topics = ["testTopic2"], groupId = "group_0", containerFactory = "kafkaConsumerForTest")
+    @KafkaListener(topics = ["testTopic2"], containerFactory = "kafkaConsumerForTestGroup1")
     fun testTopic2Group0Listener(data: Any?) {
-        classLogger.info(">> testTopic2 group_0 : $data")
+        classLogger.info(">> testTopic2 group_1 : $data")
     }
 
     // (testTopic2 에 대한 동일 그룹 테스트 리스너)
     // 동일 topic 에 동일 group 을 설정할 경우, 리스너는 한개만을 선택하고 다른 하나는 침묵합니다.
     // containerFactory 에 kafka consumer container factory 이름 - KafkaConsumerConfig 의 Bean 함수명을 입력하세요
-    @KafkaListener(topics = ["testTopic2"], groupId = "group_0", containerFactory = "kafkaConsumerForTest")
+    @KafkaListener(topics = ["testTopic2"], containerFactory = "kafkaConsumerForTestGroup1")
     fun testTopic2Group0Listener2(data: Any?) {
-        classLogger.info(">> testTopic2 group_0 2 : $data")
+        classLogger.info(">> testTopic2 group_1 2 : $data")
     }
 
     // (testTopic2 에 대한 리스너 - 그룹 변경)
     // containerFactory 에 kafka consumer container factory 이름 - KafkaConsumerConfig 의 Bean 함수명을 입력하세요
-    @KafkaListener(topics = ["testTopic2"], groupId = "group_1", containerFactory = "kafkaConsumerForTest")
+    @KafkaListener(topics = ["testTopic2"], containerFactory = "kafkaConsumerForTestGroup2")
     fun testTopic2Group1Listener(data: Any?) {
-        classLogger.info(">> testTopic2 group_1 : $data")
+        classLogger.info(">> testTopic2 group_2 : $data")
     }
 }
