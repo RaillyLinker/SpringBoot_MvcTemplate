b8ae96465d7e7e6c787fcb992f47dfef621301b0
RaillyLinker <raillylinker@gmail.com>
2024-09-28 22:56:59 +0900

    Redis 클러스터 기능 구현

--- src/main/kotlin/com/raillylinker/springboot_mvc_template/configurations/redis_configs/Redis1MainConfig.kt
+++ src/main/kotlin/com/raillylinker/springboot_mvc_template/configurations/redis_configs/Redis1MainConfig.kt
@@ -1,10 +1,13 @@
 package com.raillylinker.springboot_mvc_template.configurations.redis_configs
 
+import io.lettuce.core.SocketOptions
+import io.lettuce.core.cluster.ClusterClientOptions
+import io.lettuce.core.cluster.ClusterTopologyRefreshOptions
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.RedisClusterConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
@@ -26,21 +29,56 @@ class Redis1MainConfig {
     }
 
     // ---------------------------------------------------------------------------------------------
-    @Value("\${datasource-redis.${REDIS_CONFIG_NAME}.host}")
-    private lateinit var redisHost: String
-
-    @Value("\${datasource-redis.${REDIS_CONFIG_NAME}.port}")
-    private var redisPort: Int? = null
+    @Value("\${datasource-redis.${REDIS_CONFIG_NAME}.node-list:}#{T(java.util.Collections).emptyList()}")
+    private lateinit var nodeList: List<String>
 
     @Bean(REDIS_CONFIG_NAME + "_ConnectionFactory")
     fun redisConnectionFactory(): LettuceConnectionFactory {
-        val clientConfig = LettuceClientConfiguration.builder()
-//            .useSsl().and()
-            .commandTimeout(Duration.ofSeconds(2))
-            .shutdownTimeout(Duration.ZERO)
+        // todo 주석 추가
+
+        //----------------- (1) Socket Option
+        val socketOptions: SocketOptions = SocketOptions.builder()
+            .connectTimeout(Duration.ofMillis(3000L))
+            .keepAlive(true)
             .build()
 
-        return LettuceConnectionFactory(RedisStandaloneConfiguration(redisHost, redisPort!!), clientConfig)
+
+        //----------------- (2) Cluster topology refresh 옵션
+        val clusterTopologyRefreshOptions = ClusterTopologyRefreshOptions
+            .builder()
+            .dynamicRefreshSources(true)
+            .enableAllAdaptiveRefreshTriggers()
+            .enablePeriodicRefresh(Duration.ofSeconds(30))
+            .build()
+
+
+        //----------------- (3) Cluster client 옵션
+        val clusterClientOptions = ClusterClientOptions
+            .builder()
+            .pingBeforeActivateConnection(true)
+            .autoReconnect(true)
+            .socketOptions(socketOptions)
+            .topologyRefreshOptions(clusterTopologyRefreshOptions)
+            .maxRedirects(3).build()
+
+
+        //----------------- (4) Lettuce Client 옵션
+        val clientConfig = LettuceClientConfiguration
+            .builder()
+            .commandTimeout(Duration.ofMillis(3000L))
+            .clientOptions(clusterClientOptions)
+            .build()
+
+        val clusterConfig: RedisClusterConfiguration = RedisClusterConfiguration(nodeList)
+        clusterConfig.maxRedirects = 3
+//        clusterConfig.setPassword("password")
+
+        val factory = LettuceConnectionFactory(clusterConfig, clientConfig)
+
+        //----------------- (5) LettuceConnectionFactory 옵션
+        factory.validateConnection = false
+
+        return factory
     }
 
     @Bean(REDIS_TEMPLATE_NAME)
@@ -48,6 +86,7 @@ class Redis1MainConfig {
         val redisTemplate = RedisTemplate<String, String>()
         redisTemplate.connectionFactory = redisConnectionFactory()
         redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = StringRedisSerializer()
         return redisTemplate
     }
 }
